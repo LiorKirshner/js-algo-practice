c84c39a165eda5b5108306c66ea0077c58374ac2
LiorKirshner <liorkk7@gmail.com>
2025-07-15 09:05:46 +0300

    factor chain

--- Easy/factorChain.js
+++ Easy/factorChain.js
@@ -18,8 +18,12 @@ factorChain([2, 4, 6, 7, 12]) ➞ false
 factorChain([10, 1]) ➞ false
 */
 
-function factorChain( /*args*/ ) {
-  //your code
+function factorChain(arr) {
+  if (arr.length == 1) return true; //array with one number
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] % arr[i - 1] != 0) return false;
+  }
+  return true;
 }
 
 exports.solution = factorChain;
