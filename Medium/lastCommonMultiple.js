828ea05c8104269bdae202fb2c361858a5eecd3e
LiorKirshner <liorkk7@gmail.com>
2025-08-05 17:07:43 +0300

    sul using gcd

--- Medium/lastCommonMultiple.js
+++ Medium/lastCommonMultiple.js
@@ -9,11 +9,22 @@ lcm(8, 5) ➞ 40
 lcm(17, 11) ➞ 187
 Notes
 Both values will be positive.
-The LCM is the smallest integer that divides both numbers such that the remainder is zero.
+The LCM is the smallest positive integer that is evenly divisible by both a and b.
 */
 
-function lastCommonMultiple(/*args*/) {
-  //your code
+function lastCommonMultiple(num1, num2) {
+  // פונקציה למציאת המחלק המשותף הגדול ביותר (GCD)
+  function gcd(a, b) {
+    while (b !== 0) {
+      let temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
+  }
+
+  // חישוב LCM
+  return (num1 * num2) / gcd(num1, num2);
 }
 
 exports.solution = lastCommonMultiple;
